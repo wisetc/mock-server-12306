04d6134cc6641e52a57dbf6b603c7ca1f53c8bf0
王志 <zhi@uqugu.com>
2019-01-03 17:14:03 +0800

    feat: add checkUser

--- server/routes.js
+++ server/routes.js
@@ -13,6 +13,8 @@ function respondJson(jsonName) {
 function makeRoutes(server) {
   server.get('/otn/leftTicket/queryZ', respondJson('queryZ'));
   server.get('/otn/leftTicket/queryTicketPrice', respondJson('queryTicketPrice'));
+  server.post('/otn/login/checkUser', respondJson('checkUser'));
+  server.post('/otn/leftTicket/submitOrderRequest', respondJson('submitOrderRequest'));
 }
 
 module.exports = makeRoutes;
